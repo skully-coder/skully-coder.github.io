a10cd7f31a0ca5fd918d79954c754a49bf94c15f
Abhinav Agrawal <55917548+skully-coder@users.noreply.github.com>
2023-01-05 07:39:11 +0000

    Added Education

---
+++ src/Components/Education.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Education = () => {
+  return (
+    <section className="light" id="education">
+      <h2>Education</h2>
+      <ul>
+        <li>
+          <div>
+            <h3 className="education">Manipal Institute of Technology</h3>
+            <p>
+              Bachelor&apos;s of Technology, Computer Science and Engineering &nbsp; &nbsp; &nbsp;
+              &nbsp; <b>2019-2023</b>
+            </p>
+          </div>
+        </li>
+        <br></br>
+        <li>
+          <div>
+            <h3 className="education">Hiranandani Foundation School, Thane</h3>
+            <p>
+              Grade 12 &nbsp; &nbsp; &nbsp; &nbsp;<b>2017-2019</b>
+            </p>
+          </div>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default Education;
